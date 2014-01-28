3f747b18f081d3acccaa81f338abd17a19170aaa
Josh Smith <joshdotsmith@gmail.com>
2014-01-28 00:41:25 -0800

    Add quick image removal

--- app/views/draggable_view.rb
+++ app/views/draggable_view.rb
@@ -28,7 +28,7 @@ class DraggableView < UIView
   
   def drag_gesture(panGesture)
     translation = panGesture.translationInView(self.superview)
-    
+
     case panGesture.state
     when UIGestureRecognizerStateBegan
       @originalCenter = self.center
@@ -37,8 +37,9 @@ class DraggableView < UIView
                                      self.center.y + translation.y)
       change_in_x = @originalCenter.x - self.center.x
       abs_change_in_x = change_in_x.abs
+
       rmq(self).style do |st|
-        st.rotation = change_in_x * 0.11
+        st.rotation = change_in_x * 0.09
       end
       rmq(@liked).style do |st|
         if change_in_x < 0
@@ -52,8 +53,36 @@ class DraggableView < UIView
       end
       puts "Absolute change in x: #{abs_change_in_x}"
     when UIGestureRecognizerStateEnded
+      self.center = CGPointMake(self.center.x + translation.x,
+                                     self.center.y + translation.y)
+      change_in_x = @originalCenter.x - self.center.x
+      abs_change_in_x = change_in_x.abs
+
       if (@destinationView && CGRectContainsRect(@destinationView.frame, self.frame))
         self.removeGestureRecognizer(@panGesture)        
+      elsif abs_change_in_x >= 90
+        if change_in_x > 0
+          puts "disliked"
+          UIView.animateWithDuration(0.5,
+           animations:lambda {
+             self.center = CGPointMake(-1000,
+                                     self.center.y + translation.y)
+             },
+            completion:lambda {|finished|
+
+           })
+
+        else
+          puts "liked"
+          UIView.animateWithDuration(0.5,
+           animations:lambda {
+             self.center = CGPointMake(1000,
+                                     self.center.y + translation.y)
+             },
+            completion:lambda {|finished|
+
+           })
+        end
       else
         UIView.animateWithDuration(0.5,
              animations:lambda {
